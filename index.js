17b7e227ca2ab1ac4bb20293b488a16a4425637e
Deena Faria <deenafaria@gmail.com>
2024-09-29 10:56:30 +0600

    lll

--- index.js
+++ index.js
@@ -140,6 +140,7 @@ socket.on('get_presentations', () => {
     });
 });
 
-server.listen(3001, () => {
-    console.log('Server running on http://localhost:3001');
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
